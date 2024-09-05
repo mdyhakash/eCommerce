2f67a54c6fbd477c06d24fba576747ddad7a40ad
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2024-09-05 09:59:10 +0600

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Order from "./pages/Order";
+import PlaceOrder from "./pages/PlaceOrder";
+import Products from "./pages/Products";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Order />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
