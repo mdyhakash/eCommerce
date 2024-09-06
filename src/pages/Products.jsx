917d17f2dbed4f37cae46a4422a7fae028a6b196
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2024-09-07 00:44:15 +0600

    Cart,PlaceOrder pages completed and added dunctionality to those pages

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,7 +5,7 @@ import { ShopContext } from "../context/ShopContext";
 import RelatedProduct from "../components/RelatedProduct";
 const Products = () => {
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency,addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, setSize] = useState("");
@@ -80,7 +80,7 @@ const Products = () => {
               ))}
             </div>
           </div>
-          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+          <button onClick={()=>addToCart(productData._id, size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
             Add to cart
           </button>
           <hr className="mt-8 sm:w-4/5" />
