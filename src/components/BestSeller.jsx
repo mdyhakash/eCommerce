a5fca0ebde1a211bcfe7d9ca5f10432b5274750d
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2024-09-06 09:41:36 +0600

    Deploy error fix

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import ProductItem from "./ProductItem";
 import Title from "./Title";
-import ProductItem from "./productItem";
 
 const BestSeller = () => {
   const { products } = useContext(ShopContext);
@@ -22,11 +22,15 @@ const BestSeller = () => {
         </p>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {
-            bestSeller.map((item,index)=>(
-                <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
-            ))
-        }
+        {bestSeller.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
       </div>
     </div>
   );
